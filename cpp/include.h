4929fe847afbbf5cced87724949a07f0b5e4c8cb
jj4jj <resc@vip.qq.com>
2014-10-20 16:56:57 +0800

    add include

--- cpp/include.h
+++ cpp/include.h
@@ -1,6 +1,6 @@
 #pragma once
 //protobufer generate code include file . don't edit it !
-//generate time :2014-10-19 00:39:44.378000
+//generate time :2014-10-20 16:53:11.327000
 #include "demo.pb.h"
 namespace res
 {
